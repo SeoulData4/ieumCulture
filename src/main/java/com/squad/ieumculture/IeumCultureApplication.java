a54310f732e08ab4fefd4c0fcf9f642d578401b0
wisdom5565 <wisdom5565@naver.com>
2023-06-19 22:40:10 +0900

    [spring boot] 기본세팅 커밋

---
+++ src/main/java/com/squad/ieumculture/IeumCultureApplication.java
@@ -0,0 +1,13 @@
+package com.squad.ieumculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeumCultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeumCultureApplication.class, args);
+    }
+
+}

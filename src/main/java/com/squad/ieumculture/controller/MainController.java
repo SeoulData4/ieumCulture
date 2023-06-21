5ec47ed6163dc7ff904febc598172a76b54a1d5e
wisdom5565 <wisdom5565@naver.com>
2023-06-21 22:29:45 +0900

    [member,header, footer]  css수정, service, repository 등 수정

--- src/main/java/com/squad/ieumculture/controller/MainController.java
+++ src/main/java/com/squad/ieumculture/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.squad.ieumculture.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,19 +19,30 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("header")
-    public String header (ModelMap map){
-        return "/fragment/header";
-    }
-    @GetMapping("footer")
-    public String footer (ModelMap map){
-        return "/fragment/footer";
+    @GetMapping("login")
+    public String login (Model model) {
+        return "login";
     }
 
-    @GetMapping("login")
-    public ModelAndView login () {
-            return new ModelAndView("/login");
+    @PostMapping("/loginOk")
+    public String loginOk(HttpServletRequest request,String memId, String memPw, Model model) {
+
+        return "/";
+    }
+
+/*    @PostMapping(path="/loginOk")  // http://localhost:8888/loginOk
+    public String loginOk(HttpServletRequest request, @RequestParam String mem_id, String adUserpw){
+        if(adminLogicService.read(adUserid,adUserpw).getData()!= null) {
+            HttpSession session = request.getSession();
+            String name = adminLogicService.read(adUserid, adUserpw).getData().getAdName();
+            String userid = adminLogicService.read(adUserid, adUserpw).getData().getAdUserid();
+            session.setAttribute("userid",userid);
+            session.setAttribute("name",name);
+            return "redirect:/";
+        }else{
+            return "redirect:/login";
         }
+    }*/
     @GetMapping("join")
     public String join1() {
         return "joinAgree";
